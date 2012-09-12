b7b0754b49a9a3443d774d407782378c1042e732
dodikk <dodikk88.reg@gmail.com>
2012-09-12 13:13:46 +0300

    Added missing includes

--- ESDatabaseWrapper/ESDatabaseWrapper.h
+++ ESDatabaseWrapper/ESDatabaseWrapper.h
@@ -2,6 +2,9 @@
 #import <ESDatabaseWrapper/ESQueriedSet.h>
 #import <ESDatabaseWrapper/ESReadOnlyDbWrapper.h>
 #import <ESDatabaseWrapper/ESWritableDbWrapper.h>
-#import <ESDatabaseWrapper/FMDatabase+Wrapper.h>
 #import <ESDatabaseWrapper/ESQueriedSet.h>
 #import <ESDatabaseWrapper/ESQueriedSet.h>
+#import <ESDatabaseWrapper/SqliteTypes.h>
+
+#import <ESDatabaseWrapper/Mock/MockReadableDb.h>
+#import <ESDatabaseWrapper/Mock/MockWriteableDb.h>
