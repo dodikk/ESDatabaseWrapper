c9b168beefdedbf090419fc21848522bfa31e1f4
dodikk <dodikk88.reg@gmail.com>
2012-09-12 11:53:04 +0300

    Initial Commit

---
+++ ESDatabaseWrapper/ESDatabaseWrapper.h
@@ -0,0 +1,13 @@
+//
+//  ESDatabaseWrapper.h
+//  ESDatabaseWrapper
+//
+//  Created by Oleksandr Dodatko on 9/12/12.
+//  Copyright (c) 2012 EmbeddedSources. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ESDatabaseWrapper : NSObject
+
+@end
---
+++ ESDatabaseWrapper/ESDatabaseWrapper.m
@@ -0,0 +1,13 @@
+//
+//  ESDatabaseWrapper.m
+//  ESDatabaseWrapper
+//
+//  Created by Oleksandr Dodatko on 9/12/12.
+//  Copyright (c) 2012 EmbeddedSources. All rights reserved.
+//
+
+#import "ESDatabaseWrapper.h"
+
+@implementation ESDatabaseWrapper
+
+@end
