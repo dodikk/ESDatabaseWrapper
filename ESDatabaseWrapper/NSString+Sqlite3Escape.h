bcfee6e6ae634d216db72caf6db7291258f6da34
dodikk <dodikk88.reg@gmail.com>
2012-09-13 12:13:44 +0300

    added null handling category

--- ESDatabaseWrapper/NSString+Sqlite3Escape.h
+++ ESDatabaseWrapper/NSString+Sqlite3Escape.h
@@ -2,6 +2,7 @@
 
 @interface NSString (Sqlite3Escape)
 
--(NSString*)sqlite3Escape;
++(NSString*)sqlite3EscapeString:( NSString* )str_;
++(NSString*)sqlite3QuotedStringOrNull:( NSString* )str_;
 
 @end
--- ESDatabaseWrapper/NSString+Sqlite3Escape.m
+++ ESDatabaseWrapper/NSString+Sqlite3Escape.m
@@ -2,6 +2,26 @@
 
 @implementation NSString (Sqlite3Escape)
 
++(NSString*)sqlite3QuotedStringOrNull:( NSString* )str_
+{
+    if ( nil == str_ )
+    {
+        return @"null";
+    }
+
+    return [ NSString stringWithFormat: @"'%@'", [ self sqlite3EscapeString: str_ ] ];
+}
+
++(NSString*)sqlite3EscapeString:( NSString* )str_
+{
+    if ( nil == str_ )
+    {
+        return @"null";
+    }
+
+    return [ str_ sqlite3Escape ];
+}
+
 -(NSString*)sqlite3Escape
 {
     const char * cStrResult_ = [ self cStringUsingEncoding: NSUTF8StringEncoding ];
