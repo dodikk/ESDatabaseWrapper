d0f7ac8de503807bf50a68bfb64186f2dc4cf98b
dodikk <dodikk88.reg@gmail.com>
2014-12-06 19:25:58 +0200

    [carthage] Added a dynamic framework target and scheme

---
+++ ESDatabaseWrapper-DynamicFramework/ESDatabaseWrapper-DynamicFramework.h
@@ -0,0 +1,19 @@
+//
+//  ESDatabaseWrapper-DynamicFramework.h
+//  ESDatabaseWrapper-DynamicFramework
+//
+//  Created by Oleksandr Dodatko on 06/12/2014.
+//  Copyright (c) 2014 EmbeddedSources. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ESDatabaseWrapper-DynamicFramework.
+FOUNDATION_EXPORT double ESDatabaseWrapper_DynamicFrameworkVersionNumber;
+
+//! Project version string for ESDatabaseWrapper-DynamicFramework.
+FOUNDATION_EXPORT const unsigned char ESDatabaseWrapper_DynamicFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESDatabaseWrapper_DynamicFramework/PublicHeader.h>
+
+
